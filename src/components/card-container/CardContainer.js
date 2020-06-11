2b7fc12acd3e78f4a26f63e49b38760e3723d7e8
Robin Khiv <robinkhiv@gmail.com>
2020-06-11 00:12:42 -0700

    changed card container to component rather than function

--- src/components/card-container/CardContainer.js
+++ src/components/card-container/CardContainer.js
@@ -1,27 +1,26 @@
 import React from 'react';
 import BlogCard from './BlogContainer';
 
-const card_container = props => {
-    const data = props.data;
-    
-    return (
-        <section className="container">
-            <div className="row">
-                {data.length && 
-                data.map(blog => {
-                    if(blog.category_id){
-                        blog  = blog.blog; 
+export default class CardContainer extends React.Component {
+    render () {
+        const data = this.props.data;
+        return (
+            <section className="container">
+                <div className="row">
+                    {data.length && 
+                    data.map(blog => {
+                        if(blog.category_id){
+                            blog  = blog.blog; 
+                        }
+                    return <BlogCard 
+                        key={blog.id} 
+                        blog_id={blog.blog_id}
+                        blog_content={blog.blog_content}
+                        title={blog.title}
+                    />})
                     }
-                   return <BlogCard 
-                    key={blog.id} 
-                    blog_id={blog.blog_id}
-                    blog_content={blog.blog_content}
-                    title={blog.title}
-                />})
-                }
-            </div>         
-        </section>
-    )
+                </div>         
+            </section>
+        )
+    }
 }
-
-export default card_container;
